13c6c160de4642e7ce1b8b58a9df6b4e8ef365c5
Anne <anne@synerter.com>
2025-03-03 23:14:01 +0800

    feat:add store to all

--- src/index.js
+++ src/index.js
@@ -3,18 +3,22 @@ import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './index.css';
 import App from './App';
+//引入 Provider & store 
+import { Provider } from 'react-redux'
+import store from './redux/store'
 import reportWebVitals from './reportWebVitals';
 // 載入context
 import { ListProvider } from './hooks/use-list'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <ListProvider>
+  <Provider store = {store}>
+ <ListProvider>
   <React.StrictMode>
     <App />
   </React.StrictMode>
   </ListProvider>
-
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
